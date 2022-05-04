c59e586e4320c2c239a09200b6fbe7755f77e748
ivesfg1 <wigzfurtado@gmail.com>
2022-05-04 20:50:09 -0300

    enviando os arquivos requisitados pelo professor

---
+++ Programa2_com_nome_diferente.java
@@ -0,0 +1,8 @@
+// Escrevendo o codigo todo em Maiusculo, como requisitado pela questao 7
+
+PUBLIC CLASS PROGRAM {
+    PUBLIC STATIC VOID MAIN(STRING[] ARGS){
+        SYSTEM.OUT.PRINTLN("Ives Furtado");
+        SYSTEM.OUT.PRINTLN("Brasil");
+    }
+}
